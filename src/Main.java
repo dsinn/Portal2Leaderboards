d454ad1e067a086fbfa31f42b2c138fec7a4b34f
Daniel Sinn <daniel.p.sinn@gmail.com>
2013-10-16 16:31:37 -0400

    Just pushing what I have

---
+++ src/Main.java
@@ -0,0 +1,352 @@
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.UIManager;
+
+public class Main {
+	final public static String MAIN_URL = "http://steamcommunity.com/";
+	final public static String PORTAL2_STATS = "/stats/Portal2?tab=leaderboards";
+
+	final public static String DEFAULT_FILE = "Portal2_leaderboards.html";
+	final public static String START_FILE = "start.txt";
+	final public static String END_FILE = "end.txt";
+
+	final public static String TZ = new SimpleDateFormat("z")
+			.format(new Date());
+	final public static String URL_ENCODING = "UTF-8";
+	final public static String N_A = "\u2013";
+	final public static String TIE_PREFIX = "T-";
+
+	public static void main(String[] args) throws IOException {
+		final Scanner snInput = new Scanner(System.in);
+		System.out.print("Enter your Steam Community ID: ");
+		final String input = snInput.nextLine();
+		final String subpage = (input.matches("7656119\\d{10}") ? "profiles"
+				: "id") + "/" + input;
+		final Scanner snParser = new Scanner(new URL(MAIN_URL + subpage
+				+ PORTAL2_STATS).openStream(), URL_ENCODING);
+		final LinkedHashMap<String, String> lbs = getLeaderboards(snParser);
+		activateSelector(lbs, subpage);
+	}
+
+	protected static LinkedHashMap<String, String> getLeaderboards(Scanner sn) {
+		final LinkedHashMap<String, String> lbs = new LinkedHashMap<String, String>();
+		final Pattern p = Pattern
+				.compile(
+						"<option value=\"(\\d+)\"(?: selected=\"[^\"]+\")?>([^<]+)</option>",
+						Pattern.CASE_INSENSITIVE);
+
+		String line;
+		while (!(line = sn.nextLine()).contains("<select name=\"lb\""))
+			;
+		while (!(line = sn.nextLine()).contains("</select>")) {
+			final Matcher m = p.matcher(line);
+			m.find();
+			lbs.put(m.group(1), m.group(2));
+		}
+		return lbs;
+	}
+
+	/**
+	 * Return the input but with invalid characters removed according to the
+	 * HTML ID attribute specification.
+	 * 
+	 * @param s
+	 *            Input ID
+	 * @return Input but with invalid characters removed according to the HTML
+	 *         ID attribute specification.
+	 */
+	public static String idify(String s) {
+		return s.replaceAll("[^0-9A-Za-z_]", "");
+	}
+
+	/**
+	 * Output the selected leaderboards to a file.
+	 * 
+	 * @param lbs
+	 *            all leaderboards' ID-name pairs
+	 * @param subpage
+	 *            the relative path of the Steam profile
+	 * @param subset
+	 *            the selected leaderboards' IDs
+	 */
+	public static void outputLBs(LinkedHashMap<String, String> lbs,
+			String subpage, List<String> subset) {
+		try {
+			final File file = new File(DEFAULT_FILE);
+			final FileOutputStream fso = new FileOutputStream(file);
+			final OutputStreamWriter osw = new OutputStreamWriter(fso,
+					URL_ENCODING);
+			final BufferedWriter bw = new BufferedWriter(osw);
+			bw.write(copypaste(START_FILE));
+			final Pattern divPattern = Pattern.compile("<(/?)div[>\\s]",
+					Pattern.CASE_INSENSITIVE);
+
+			// Date
+			bw.write(String
+					.format("<p>Loaded on %s<script type=\"text/javascript\">document.write(' (' + dateDiff(%d) + ')');</script>.</p>",
+							new Date(),
+							System.currentTimeMillis()
+									+ TimeZone.getDefault().getOffset(
+											System.currentTimeMillis())));
+			bw.newLine();
+
+			final String[][] anchorData = new String[subset.size()][2];
+			int count = 0;
+			for (final String key : subset) {
+				anchorData[count][0] = N_A;
+				anchorData[count][1] = N_A;
+
+				final String title = lbs.get(key);
+				final String url = MAIN_URL + subpage + PORTAL2_STATS + "&lb="
+						+ key;
+				final Scanner sn = new Scanner(new URL(url).openStream(),
+						URL_ENCODING);
+
+				// Find beginning of LB entries
+				String line;
+				while (!(line = sn.nextLine()).contains(" class=\"lbentry\"")
+						&& sn.hasNextLine())
+					;
+				if (sn.hasNextLine()) {
+					// Leaderboard is not empty
+					bw.write(String
+							.format("<h2 id=\"%s\"><a href=\"%s\" target=\"_blank\">%s</a></h2>%n",
+									idify(title), url.replaceAll("&", "&amp;"),
+									title));
+					bw.write("<table class=\"lb\">");
+					bw.newLine();
+
+					String myScore = "", lastScore = "", lastScoreRank = null;
+					while (!line.contains("<!-- footer -->")) {
+						if ((line.contains(" class=\"lbentry\""))) {
+							int divCount = 1;
+							String entryData = "";
+							while (divCount > 0) {
+								line = sn.nextLine();
+								final Matcher m = divPattern.matcher(line);
+								entryData += line;
+								while (m.find()) {
+									divCount += m.group(1).equals("") ? 1 : -1;
+								}
+							}
+
+							final Map<String, String> entryMap = new HashMap<String, String>();
+							for (final String divHtml : entryData.trim().split(
+									"<div\\s[^>]*?class=\"")) {
+								final int iValueEnd = divHtml.indexOf("</div>");
+								if (iValueEnd == -1) {
+									continue;
+								}
+
+								final int iQuote = divHtml.indexOf(divHtml
+										.indexOf('"'));
+								final int iValueStart = divHtml.indexOf('>',
+										iQuote + 1) + 1;
+								final String className = divHtml.substring(0,
+										divHtml.indexOf('"'));
+								final String value = divHtml.substring(
+										iValueStart, iValueEnd);
+								entryMap.put(className, value);
+							}
+
+							if (entryMap.containsKey("rRh")) {
+								// Found myself
+								myScore = entryMap.get("scoreh");
+								anchorData[count][0] = myScore
+										.equals(lastScore) ? TIE_PREFIX
+										+ lastScoreRank : entryMap.get("rRh")
+										.replaceAll("\\D", "").trim();
+								String globalRank = entryMap.get("globalRankh")
+										.replaceFirst("Global rank: #", "")
+										.trim();
+								anchorData[count][1] = addTargetBlank(globalRank);
+
+								writeRow(bw, entryMap, "h", new String[] {
+										"rRh", "avatarIcon", "playerLink",
+										"scoreh", "globalRankh" });
+							} else {
+								// Somebody else
+								if (!lastScore.equals(entryMap.get("score"))) {
+									lastScore = entryMap.get("score");
+									lastScoreRank = entryMap.get("rR")
+											.replaceAll("\\D", "");
+								}
+								if (lastScore.equals(myScore)
+										&& !anchorData[count][0]
+												.startsWith(TIE_PREFIX)) {
+									anchorData[count][0] = TIE_PREFIX
+											+ anchorData[count][0];
+								}
+
+								writeRow(bw, entryMap, null, new String[] {
+										"rR", "avatarIcon", "playerLink",
+										"score", "globalRank" });
+							}
+						}
+						line = sn.nextLine();
+					}
+
+					bw.write("</table>");
+					bw.newLine();
+					System.out.printf("Read %d/%d: %s%n", ++count,
+							subset.size(), title);
+				} else {
+					System.out.printf("No entries found for %d/%d: %s%n",
+							++count, subset.size(), title);
+				}
+			}
+
+			addAnchorList(bw, lbs, subset, anchorData);
+			bw.write(copypaste(END_FILE));
+			bw.close();
+			java.awt.Desktop.getDesktop().browse(file.toURI());
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	protected static void addAnchorList(BufferedWriter bw,
+			LinkedHashMap<String, String> lbs, List<String> subset,
+			String[][] anchorData) throws IOException {
+		bw.write("<div class=\"anchorList\"><table>");
+		bw.newLine();
+		bw.write("<tr><th>Leaderboard</th>");
+		bw.newLine();
+		bw.write("<th><abbr title=\"Relative rank (amongst friends)\">RR</abbr></th>");
+		bw.newLine();
+		bw.write("<th><abbr title=\"Global rank\">GR</abbr></th></tr>");
+		bw.newLine();
+
+		for (int i = 0; i < subset.size(); i++) {
+			final String title = lbs.get(subset.get(i));
+			bw.write(String
+					.format("<tr><td><a href=\"#%s\">%s</a></td><td>%s</td><td>%s</td></tr>%n",
+							idify(title), title, anchorData[i][0],
+							anchorData[i][1]));
+		}
+		bw.write("</table></div>");
+		bw.newLine();
+	}
+
+	/**
+	 * Returns all of the text in a file.
+	 * 
+	 * @param path
+	 *            the path of the file
+	 * @return all of the text in a file
+	 */
+	protected static String copypaste(String path) {
+		String output = "";
+		final Scanner sn = new Scanner(ClassLoader.getSystemClassLoader()
+				.getResourceAsStream(path));
+		while (sn.hasNextLine()) {
+			output += String.format("%s%n", sn.nextLine());
+		}
+		return output;
+	}
+
+	public static String addTargetBlank(String src) {
+		final int aLeft = src.indexOf("<a ");
+		if (aLeft == -1) {
+			return src;
+		}
+
+		final int aRight = src.indexOf(">", aLeft + 2);
+		final String aHtml = src.substring(aLeft, aRight);
+		if (aHtml.contains("target")) {
+			return src;
+		}
+
+		return src.substring(0, aRight) + " target=\"_blank\""
+				+ src.substring(aRight);
+	}
+
+	/**
+	 * Writes TR HTML to the BufferedWriter.
+	 * 
+	 * @param bw
+	 *            the BufferedWriter
+	 * @param map
+	 *            the class-value pairs
+	 * @param trClass
+	 *            class for the TR element; null if no class
+	 * @param keys
+	 *            the names of the classes whose data should be printed in the
+	 *            TD elements.
+	 * @throws IOException
+	 */
+	public static void writeRow(BufferedWriter bw, Map<String, String> map,
+			String trClass, String[] keys) throws IOException {
+		bw.write("<tr" + (trClass == null ? "" : " class=\"" + trClass + "\"")
+				+ ">");
+		bw.newLine();
+		for (final String key : keys) {
+			String s = map.get(key);
+			// W3 standards
+			s = s.replaceAll("<img src=\"", "<img alt=\"\" src=\"");
+
+			String output = "";
+			int iLeft, iRight = 0;
+			while ((iLeft = s.indexOf("<a class=\"playerName\"", iRight)) > 0) {
+				output += s.substring(iRight, iLeft);
+				iRight = s.indexOf("</a>", iLeft);
+				final int iName = s.lastIndexOf('>', iRight) + 1;
+				output += s.substring(iLeft, iName)
+						+ s.substring(iName, iRight).replaceAll("&", "&amp;")
+								.replaceAll("<", "&lt;")
+								.replaceAll(">", "&gt;")
+								.replaceAll("\"", "&quot;");
+			}
+
+			bw.write("<td>" + output + s.substring(iRight) + "</td>");
+			bw.newLine();
+		}
+		bw.write("</tr>");
+		bw.newLine();
+	}
+
+	/**
+	 * Opens the GUI for leaderboard selection.
+	 * 
+	 * @param lbs
+	 *            all the leaderboards' ID-name pairs
+	 * @param subpage
+	 *            the relative path of the Steam profile page
+	 */
+	public static void activateSelector(LinkedHashMap<String, String> lbs,
+			String subpage) {
+		try {
+			UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception evt) {
+		}
+
+		final SelectionGUI frame = new SelectionGUI(lbs, subpage);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.setSize(240, GraphicsEnvironment.getLocalGraphicsEnvironment()
+				.getMaximumWindowBounds().height);
+		frame.setVisible(true);
+	}
+}
---
+++ src/SelectionGUI.java
@@ -0,0 +1,344 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.ButtonGroup;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+
+public class SelectionGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+	final protected String[][] spChapters = {
+			{ "The Courtesy Call", "Portal Gun" },
+			{ "The Cold Boot", "Laser Stairs" },
+			{ "The Return", "Ceiling Catapult" },
+			{ "The Surprise", "Column Blocker" },
+			{ "The Escape", "Turret Factory" }, { "The Fall", "Underground" },
+			{ "The Reunion", "Propulsion Intro" },
+			{ "The Itch", "Funnel Intro" },
+			{ "The Part Where He Kills You", "Finale 2" } };
+	final protected String[] coopChapters = { "Team Building",
+			"Mass and Velocity", "Hard-Light Surfaces", "Excursion Funnels",
+			"Mobility Gels", "Art Therapy" };
+	final protected LinkedHashMap<String, String> lbs;
+	final protected String subpage;
+	final protected ModePanel mp;
+
+	public SelectionGUI(LinkedHashMap<String, String> lbs, String subpage) {
+		super("Portal 2 Leaderboards Selection");
+		this.lbs = lbs;
+		this.subpage = subpage;
+		final CheckNode all = new CheckNode("All");
+		final Object[] keys = lbs.keySet().toArray();
+
+		String title;
+		int i;
+		final CheckNode sp = new CheckNode("Single-player");
+		all.add(sp);
+		int spIndex = 0;
+		CheckNode currentChapter = null;
+		for (i = 0; !(title = lbs.get(keys[i])).matches("\\d+.+"); i++) {
+			if (spIndex < spChapters.length
+					&& title.startsWith(spChapters[spIndex][1])) {
+				currentChapter = new CheckNode(spChapters[spIndex++][0]);
+				sp.add(currentChapter);
+			}
+			currentChapter.add(new CheckNode(title, keys[i].toString()));
+		}
+		final CheckNode coop = new CheckNode("Cooperative");
+		all.add(coop);
+		int mpIndex = 0;
+		for (; i < keys.length; i++) {
+			title = lbs.get(keys[i]);
+			if (title.startsWith("01 ") && title.endsWith("Portals")) {
+				currentChapter = new CheckNode(coopChapters[mpIndex++]);
+				coop.add(currentChapter);
+			}
+			currentChapter.add(new CheckNode(title, keys[i].toString()));
+		}
+
+		final JTree tree = new JTree(all);
+		tree.expandPath(new TreePath(sp.getPath()));
+		tree.expandPath(new TreePath(coop.getPath()));
+		tree.setCellRenderer(new CheckRenderer());
+		tree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tree.putClientProperty("JTree.lineStyle", "Angled");
+		tree.addMouseListener(new NodeSelectionListener(tree));
+		final JScrollPane jsp = new JScrollPane(tree);
+
+		final JButton button = new JButton("Submit");
+		button.addActionListener(new ButtonActionListener(all, this));
+		final JPanel panel = new JPanel(new BorderLayout());
+		panel.add(button, BorderLayout.SOUTH);
+
+		mp = new ModePanel();
+		getContentPane().add(jsp, BorderLayout.CENTER);
+		getContentPane().add(mp, BorderLayout.NORTH);
+		getContentPane().add(panel, BorderLayout.SOUTH);
+	}
+
+	class NodeSelectionListener extends MouseAdapter {
+		final JTree tree;
+
+		NodeSelectionListener(JTree tree) {
+			this.tree = tree;
+		}
+
+		public void mouseClicked(MouseEvent e) {
+			final int row = tree.getRowForLocation(e.getX(), e.getY());
+			final TreePath path = tree.getPathForRow(row);
+			if (path != null) {
+				final CheckNode node = (CheckNode) path.getLastPathComponent();
+				final boolean isSelected = !node.isSelected();
+				node.setSelected(isSelected);
+
+				((DefaultTreeModel) tree.getModel()).nodeChanged(node);
+				tree.revalidate();
+				tree.repaint();
+			}
+		}
+	}
+
+	class ButtonActionListener implements ActionListener {
+		final private JFrame frame;
+		final private CheckNode root;
+
+		ButtonActionListener(CheckNode root, JFrame frame) {
+			this.root = root;
+			this.frame = frame;
+		}
+
+		public void actionPerformed(ActionEvent ev) {
+			final List<String> subset = new LinkedList<String>();
+			@SuppressWarnings("unchecked")
+			final Enumeration<CheckNode> e = root.breadthFirstEnumeration();
+			while (e.hasMoreElements()) {
+				final CheckNode node = e.nextElement();
+				if (node.isSelected() && node.id != null) {
+					final String title = lbs.get(node.id);
+					if (title.endsWith("Portals") && !mp.onlyTime.isSelected()
+							|| title.endsWith("Time")
+							&& !mp.onlyPortals.isSelected()) {
+						subset.add(node.id);
+					}
+				}
+			}
+			frame.setVisible(false);
+			Main.outputLBs(lbs, subpage, subset);
+		}
+	}
+}
+
+class CheckRenderer extends JPanel implements TreeCellRenderer {
+	private static final long serialVersionUID = 1L;
+	protected JCheckBox check;
+	protected TreeLabel label;
+
+	public CheckRenderer() {
+		setLayout(null);
+		add(check = new JCheckBox());
+		add(label = new TreeLabel());
+		check.setBackground(UIManager.getColor("Tree.textBackground"));
+		label.setForeground(UIManager.getColor("Tree.textForeground"));
+	}
+
+	public Component getTreeCellRendererComponent(JTree tree, Object value,
+			boolean isSelected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus) {
+		String stringValue = tree.convertValueToText(value, isSelected,
+				expanded, leaf, row, hasFocus);
+		setEnabled(tree.isEnabled());
+		check.setSelected(((CheckNode) value).isSelected());
+		label.setFont(tree.getFont());
+		label.setText(stringValue);
+		label.setSelected(isSelected);
+		label.setFocus(hasFocus);
+		return this;
+	}
+
+	public Dimension getPreferredSize() {
+		Dimension d_check = check.getPreferredSize();
+		Dimension d_label = label.getPreferredSize();
+		return new Dimension(d_check.width + d_label.width,
+				(d_check.height < d_label.height ? d_label.height
+						: d_check.height));
+	}
+
+	public void doLayout() {
+		Dimension d_check = check.getPreferredSize();
+		Dimension d_label = label.getPreferredSize();
+		int y_check = 0;
+		int y_label = 0;
+		if (d_check.height < d_label.height) {
+			y_check = (d_label.height - d_check.height) / 2;
+		} else {
+			y_label = (d_check.height - d_label.height) / 2;
+		}
+		check.setLocation(0, y_check);
+		check.setBounds(0, y_check, d_check.width, d_check.height);
+		label.setLocation(d_check.width, y_label);
+		label.setBounds(d_check.width, y_label, d_label.width, d_label.height);
+	}
+
+	public void setBackground(Color color) {
+		if (color instanceof ColorUIResource)
+			color = null;
+		super.setBackground(color);
+	}
+
+	public class TreeLabel extends JLabel {
+		private static final long serialVersionUID = 1L;
+		boolean isSelected;
+		boolean hasFocus;
+
+		public TreeLabel() {
+		}
+
+		public void setBackground(Color color) {
+			if (color instanceof ColorUIResource)
+				color = null;
+			super.setBackground(color);
+		}
+
+		public void paint(Graphics g) {
+			String str;
+			if ((str = getText()) != null) {
+				if (0 < str.length()) {
+					if (isSelected) {
+						g.setColor(UIManager
+								.getColor("Tree.selectionBackground"));
+					} else {
+						g.setColor(UIManager.getColor("Tree.textBackground"));
+					}
+					Dimension d = getPreferredSize();
+					int imageOffset = 0;
+					Icon currentI = getIcon();
+					if (currentI != null) {
+						imageOffset = currentI.getIconWidth()
+								+ Math.max(0, getIconTextGap() - 1);
+					}
+					g.fillRect(imageOffset, 0, d.width - 1 - imageOffset,
+							d.height);
+					if (hasFocus) {
+						g.setColor(UIManager
+								.getColor("Tree.selectionBorderColor"));
+						g.drawRect(imageOffset, 0, d.width - 1 - imageOffset,
+								d.height - 1);
+					}
+				}
+			}
+			super.paint(g);
+		}
+
+		public Dimension getPreferredSize() {
+			Dimension retDimension = super.getPreferredSize();
+			if (retDimension != null) {
+				retDimension = new Dimension(retDimension.width + 3,
+						retDimension.height);
+			}
+			return retDimension;
+		}
+
+		public void setSelected(boolean isSelected) {
+			this.isSelected = isSelected;
+		}
+
+		public void setFocus(boolean hasFocus) {
+			this.hasFocus = hasFocus;
+		}
+	}
+}
+
+class CheckNode extends DefaultMutableTreeNode {
+	private static final long serialVersionUID = 1L;
+	protected int selectionMode;
+	protected boolean isSelected;
+	final public String id;
+
+	public CheckNode() {
+		this(null);
+	}
+
+	public CheckNode(Object userObject) {
+		this(userObject, true, false, null);
+	}
+
+	public CheckNode(Object userObject, String id) {
+		this(userObject, true, false, id);
+	}
+
+	public CheckNode(Object userObject, boolean allowsChildren,
+			boolean isSelected, String id) {
+		super(userObject, allowsChildren);
+		this.isSelected = isSelected;
+		this.id = id;
+	}
+
+	public void setSelectionMode(int mode) {
+		selectionMode = mode;
+	}
+
+	public int getSelectionMode() {
+		return selectionMode;
+	}
+
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+		if (children != null) {
+			@SuppressWarnings("unchecked")
+			final Enumeration<CheckNode> e = children.elements();
+			while (e.hasMoreElements()) {
+				e.nextElement().setSelected(isSelected);
+			}
+		}
+	}
+
+	public boolean isSelected() {
+		return isSelected;
+	}
+}
+
+class ModePanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	final JRadioButton onlyPortals, onlyTime, both;
+
+	ModePanel() {
+		setLayout(new GridLayout(2, 2));
+		setBorder(new TitledBorder("Selection mode"));
+		ButtonGroup group = new ButtonGroup();
+		add(onlyPortals = new JRadioButton("Only Portals"));
+		add(onlyTime = new JRadioButton("Only Time"));
+		add(both = new JRadioButton("Both"));
+		group.add(onlyPortals);
+		group.add(onlyTime);
+		group.add(both);
+		both.setSelected(true);
+	}
+}
